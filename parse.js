1c7ced75d0fbd32cde57ba8d16542f80bf9469d2
Cedric Nicoloso <cnicoloso@octo.com>
2018-05-02 22:38:12 +1000

    Extract more fields

--- parse.js
+++ parse.js
@@ -3,18 +3,47 @@ const cheerio = require('cheerio')
 function parse(html) {
   const $ = cheerio.load(html)
 
-  const hikes = $('.ww-box-wrapper')
-    .map(function(index, content) {
-      return parseHikeHtml($, content)
-    }).get()
-  return hikes
+  return $('.ww-box-wrapper')
+    .map((_, content) => parseHikeHtml($, content)).get()
 }
 
-function parseHikeHtml ($, content) {
+function parseHikeHtml($, content) {
   const detailLink = $(content).find('.ww-box-htag-title a')
-  return {
+  const hikeDetailsLink = $(detailLink[0]).attr('href')
+  const leftBox = extractLeftBoxInfo($, content)
+  const rawMainImage = $(content).find('.ww-box-thumbnail-img')
+  const rawElevationImage = $(content).find('.img-box-bottom')
+  const elevationImage = $(rawElevationImage[0]).attr('src')
+  const main = {
+    objectID: hikeDetailsLink,
     name: detailLink[0].children[0].data.trim(),
-    objectID: $(detailLink[0]).attr('href'),
+    detailLink: `http://www.wildwalks.com${hikeDetailsLink}`,
+    mainImage: $(rawMainImage[0]).attr('src'),
+    elevationImage: `http://www.wildwalks.com${elevationImage}`,
+  }
+  return Object.assign({}, main, leftBox)
+}
+
+function extractLeftBoxInfo($, content) {
+  const leftBox = $(content).find('.box-left')
+  const rawAccesses = $(leftBox).find('ul')[0]
+  const accesses = $(rawAccesses).find('li')
+    .map((_, access) => {
+      const img = $(access).find('img')
+      const href = $(img).attr('src')
+      const matches = /white_([a-z]+).png/g.exec(href)
+      return matches[1]
+    }).get()
+  const rawFirstLine = $(leftBox).find('p')[0].children[0].data
+  const rawSecondLine = $(leftBox).find('p')[0].children[2].data
+  const distance = parseInt(rawFirstLine.split(' ')[0], 10)
+  const hikeType = rawFirstLine.split('m ')[1]
+  const duration = parseInt(rawSecondLine.split('m ')[0], 10)
+  return {
+    distance,
+    hikeType,
+    duration,
+    accesses,
   }
 }
 
--- tests/parse.test.js
+++ tests/parse.test.js
@@ -7,15 +7,22 @@ describe('parse ', () => {
 
     const testData = fs.readFileSync('./tests/test-data/input.html')
 
-    it('should extract all the names', function () {
+    it('should give us a total of 245 hikes', function () {
       // When
       const results = parse(testData)
 
       // Then
       expect(results).to.have.length(245)
+    })
+
+    it('should extract all the names with none being empty', function () {
+      // When
+      const results = parse(testData)
+
+      // Then
       expect(results[0].name).to.equal('Katoomba Falls Park to Cliff View Lookout')
-      results.forEach(name => {
-        expect(name).to.be.ok
+      results.forEach(hike => {
+        expect(hike.name).to.be.ok
       })
     })
 
@@ -37,6 +44,94 @@ describe('parse ', () => {
         if (index > 0) {
           expect(hike.objectID).not.to.equal( orderedHikes[index - 1].objectID)
         }
+      })
+    })
+
+    it('should give a correct detail link', function () {
+      // When
+      const results = parse(testData)
+
+      // Then
+      const expected = 'http://www.wildwalks.com/bushwalking-and-hiking-in-nsw/blue-mountains-katoomba/katoomba-falls-park-to-cliff-view-lookout.html'
+      expect(results[0].detailLink).to.equal(expected)
+      results.forEach(hike => {
+        expect(hike.detailLink).to.be.ok
+      })
+    })
+
+    it('should give a distance of 556 m for the first hike', function () {
+      // When
+      const results = parse(testData)
+
+      // Then
+      expect(results[0].distance).to.equal(556)
+      results.forEach(hike => {
+        expect(hike.distance).to.be.ok
+        expect(hike.distance).to.be.above(0)
+      })
+    })
+
+    it('should give the hike type', function () {
+      // When
+      const results = parse(testData)
+
+      // Then
+      expect(results[0].hikeType).to.equal('Return')
+      results.forEach(hike => {
+        expect(hike.hikeType).to.be.ok
+        expect(hike.hikeType).to.be.oneOf(['Return', 'One way', 'Circuit'])
+      })
+    })
+
+    it('should give a duration of 20 for the first hike', function () {
+      // When
+      const results = parse(testData)
+
+      // Then
+      expect(results[0].duration).to.equal(20)
+      results.forEach(hike => {
+        expect(hike.duration).to.be.ok
+        expect(hike.duration).to.be.above(0)
+      })
+    })
+
+    it('should give car and bus for the first hike', function () {
+      // When
+      const results = parse(testData)
+
+      // Then
+      expect(results[0].accesses).to.deep.equal([ 'car', 'bus' ])
+      results.forEach(hike => {
+        expect(hike.accesses).to.not.be.empty
+        hike.accesses.forEach(access => {
+          expect(access).to.be.oneOf(['car', 'bus', 'train', 'ferry'])
+        })
+      })
+    })
+
+    it('should give the correct main img src for the first hike', function () {
+      // When
+      const results = parse(testData)
+
+      // Then
+      const expected = 'https://lh3.googleusercontent.com/-zVtkdIfDHC0/V3Q4TNuSoMI/AAAAAAAFzSg/gy0MPTbujwUJD4MojsW-L7mqStWhHLxAwCHM/s300-c/MM_20160629_000459.JPG'
+      expect(results[0].mainImage).to.equal(expected)
+      results.forEach(hike => {
+        expect(hike.mainImage).to.be.ok
+        expect(hike.mainImage).to.match(/^https:\/\/lh\d+\.googleusercontent\.com\/.+/)
+      })
+    })
+
+    it('should give the correct elevation img src for the first hike', function () {
+      // When
+      const results = parse(testData)
+
+      // Then
+      const expected = 'http://www.wildwalks.com/wildwalks_custom/walk_terrain/nsw-bmnp-kfptcvl_simple.png'
+      expect(results[0].elevationImage).to.equal(expected)
+      results.forEach(hike => {
+        expect(hike.elevationImage).to.be.ok
+        expect(hike.elevationImage).to.match(/^https?:\/\/www\.wildwalks\.com\/wildwalks_custom\/walk_terrain\/.+\.png$/)
       })
     })
   })
